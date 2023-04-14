dec0b38122bf9200a188182d7153bd55497b2a9e
galoryzen <rjlopezgr@gmail.com>
2023-04-14 13:31:11 -0500

    feat: using typebox for middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { connectDB } from './db';
 import userRoutes from './routes/userRoutes';
 import testingRoutes from './routes/testingRoutes';
+import { TypeboxError } from 'typebox-express-middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +17,14 @@ app.use('/testing', testingRoutes);
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript and Express!');
 });
+
+app.use((error: Error | TypeboxError, req: Request, res: Response, next: NextFunction) => {
+  if(error instanceof TypeboxError) {
+    return res.status(400).json(error);
+  } else {
+    return res.status(500).json(error);
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,44 @@
 import { Router } from 'express';
 import { createUser, getUser, getAllUsers, updateUser, deleteUser } from '../controllers/userController';
+import { Type, Static } from '@sinclair/typebox';
+import { validateRequest } from 'typebox-express-middleware';
 
 const router = Router();
 
-router.get('/', getUser);
+router.get('/', validateRequest({
+    query: Type.Object({
+        email: Type.Optional(Type.String()),
+        password: Type.Optional(Type.String()),
+        userId: Type.Optional(Type.String()),
+    }),
+}), getUser);
+
 router.get('/all', getAllUsers);
-router.post('/', createUser);
-router.patch('/:id', updateUser);
-router.delete('/:id', deleteUser);
+
+router.post('/', validateRequest({
+    body: Type.Object({
+        email: Type.String(),
+        name: Type.String(),
+        password: Type.String(),
+        cellphone: Type.Number(),
+        address: Type.String(),
+    }),
+}), createUser);
+
+router.patch('/:id', validateRequest({
+    body: Type.Object({
+        email: Type.Optional(Type.String()),
+        name: Type.Optional(Type.String()),
+        password: Type.Optional(Type.String()),
+        cellphone: Type.Optional(Type.Number()),
+        address: Type.Optional(Type.String()),
+    }),
+}), updateUser);
+
+router.delete('/:id', validateRequest({
+    params: Type.Object({
+        id: Type.String(),
+    }),
+}), deleteUser);
 
 export default router;
